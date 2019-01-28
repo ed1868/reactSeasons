596ff379392f7b32b1f381a07b069970ce040f90
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-01-27 19:39:31 -0500

    geolocation function added to app component

---
+++ src/seasonDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class seasonDisplay extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Season Display</h1>
+      </div>
+    )
+  }
+}
+
+
